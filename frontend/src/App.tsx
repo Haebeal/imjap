17f0747a8da6cb88b4b2e4de59e6a44a4f751112
nas <vividnasubi@gmail.com>
2024-08-11 18:35:17 +0900

    fix: input要素の値とbase64imageの初期化

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const App = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [base64Image, setBase64Image] = useState<string | null>(null);
   const handleInputFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
@@ -42,6 +43,11 @@ const App = () => {
       }
     } catch(err) {
       console.error("アップロードエラー", err);
+    } finally {
+      setBase64Image(null)
+      if (inputRef.current) {
+        inputRef.current.value = "";
+      }
     }
   }
 
@@ -51,6 +57,7 @@ const App = () => {
         type="file"
         accept="image/jpeg, image/png"
         onChange={handleInputFile}
+        ref={inputRef}
       />
       {base64Image && (
         <div>
