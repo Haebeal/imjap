ab7c9cc0566df20131ac533a634008365c0e51df
nas <vividnasubi@gmail.com>
2024-08-11 16:21:27 +0900

    fix: リクエスト先のエンドポイント変更

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
     };
 
     try {
-      const response = await fetch('https://hoge.net/upload', {
+      const response = await fetch('http://localhost:3000/images', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
