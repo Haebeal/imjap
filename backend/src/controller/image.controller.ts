531bb89cf4e1d0c8a1ff7142c1dc407d9b9d2fdd
rom6621 <3103rom@gmail.com>
2024-08-15 21:35:10 +0900

    feat: GET /imagesエンドポイントのコントローラーを実装

--- backend/src/controller/image.controller.ts
+++ backend/src/controller/image.controller.ts
@@ -12,11 +12,17 @@ import { ImageService } from "~/service/image.service";
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
 
+  @Get()
+  async getImages() {
+    const images = await this.imageService.getImageList();
+    return images;
+  }
+
   @Get(":id")
   async getImage(@Param() params) {
-    const blob = await this.imageService.downloadFile(params.id);
-    const arrayBuffer = await blob.arrayBuffer();
-    return new StreamableFile(Buffer.from(arrayBuffer), {
+    const blob = await this.imageService.downloadImage(params.id);
+    // ファイルがダウンロードできるようStreamableFileオブジェクトに変換し、returnする
+    return new StreamableFile(Buffer.from(await blob.arrayBuffer()), {
       type: blob.type,
       disposition: "inline",
     });
